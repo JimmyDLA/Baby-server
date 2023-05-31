af6b4122363a54e59e2f28a282f13f661d22bf03
Jimmy DLA <jimmydelosangeles@gmail.com>
2023-05-31 01:04:36 -0400

    cleaned up server

--- server.js
+++ server.js
@@ -24,25 +24,32 @@ const rooms = {}
 io.on('connection', socket => {
   console.log('socket connected!', socket.id)
 
-  socket.on('join-freq', freqID => {
-    console.log('join-freq', { freqID })
+  socket.on('join-freq', data => {
+    console.log('join-freq', data)
+    const { room } = data
     // ====================== 2. Join or create room with userID ======================
 
-    if (rooms[freqID]) {
-      // Join exisiting room
-      rooms[freqID].push(socket.id)
+    if (data.parent) {
+      // Parents can only join
+      if (rooms[room]) {
+        rooms[room].push(socket.id)
+      }
     } else {
-      // Create new rooom
-      rooms[freqID] = [socket.id]
+      // Babies can only create rooms
+      rooms[room] = [socket.id]
     }
     /*
         If both initiating and receiving peer joins the room,
         we will get the other user details.
         For initiating peer it would be receiving peer and vice versa.
     */
-    const otherUserID = rooms[freqID].find(id => id !== socket.id)
-    console.log(rooms)
-    console.log({ otherUserID })
+    let otherUserID = ''
+    console.log({ rooms })
+    if (rooms[room]) {
+      otherUserID = rooms[room].find(id => id !== socket.id)
+      console.log(rooms)
+      console.log({ otherUserID })
+    }
     // ====================== 3. If there is other user, emit to JoinFreq ======================
     if (otherUserID) {
       socket.emit('other-user', otherUserID)
@@ -75,8 +82,13 @@ io.on('connection', socket => {
       io.to(incoming.target).emit('ice-candidate', incoming)
     })
 
-    socket.on('end', incoming => {
+    socket.on('end', id => {
       console.log('[SERVER] end')
+      console.log(`[SERVER] ${rooms[id]}`)
+      delete rooms[id]
+      console.log(`[SERVER]`, { rooms })
+
+      // closeRoom(id)
       io.emit('end')
     })
 
@@ -89,8 +101,6 @@ io.on('connection', socket => {
       console.log('[SERVER] Toggle audio')
       io.emit('toggle-audio')
     })
-    // socket.emit('user', userId)
-    // socket.to(freqID).emit('user-connected', userId)
   })
 })
 
